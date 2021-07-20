a9acedbbe71526299558cd6e85977bae4f719817
Christina Yarochkina <christina.yarochkina@gmail.com>
2021-07-20 15:20:35 +0300

    changes

--- Code tasks/Ozon/numbers.py
+++ Code tasks/Ozon/numbers.py
@@ -1,9 +1,10 @@
 ## 1. Напишите функцию на python. На вход подается массив целых чисел nums. На выходе должно быть кол-во совпадающих пар чисел.
 
-nums = [int(input()) for i in range(int(input()))]
+n = int(input())
+nums = [int(input()) for i in range(n)]
 counter = 0
-for i in range(len(numbers)):
-    for j in range(i + 1, len(numbers)):
-        if numbers[i] == numbers[j]:
+for i in range(len(nums)):
+    for j in range(i + 1, len(nums)):
+        if nums[i] == nums[j]:
             counter += 1
 print(counter)
